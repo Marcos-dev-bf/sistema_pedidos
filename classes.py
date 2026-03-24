0fd97e78b4ff8240c3415964b4930551ebcc5a4d
Marcos Guilherme <Guivitorio0@gmail.com>
2026-03-24 18:32:44 -0300

    Base inicial

---
+++ classes.py
@@ -0,0 +1,34 @@
+class Produto:
+    def __init__(self,nome, preco, quantidade):
+        self.nome = nome
+        self.preco = preco
+        self.quantidade = quantidade
+
+class Carrinho:
+
+    def adicionar_produto(self):
+        pass
+
+    def remover_produto(self):
+        pass
+    
+    def listar_produto(self):
+        pass
+    
+    def calcular_total(self):
+        pass
+
+    def buscar_produto(self):
+        pass
+    pass
+
+class Pedido:
+    def gerar_resumo(self):
+        pass
+    
+    def mostrar_produto(self):
+        pass
+
+    def exibir_total(self):
+        pass
+    pass
